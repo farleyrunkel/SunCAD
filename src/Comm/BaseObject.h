bfcb215701680cc94eaf7a4e51dddd5eb24320df
Farley Runkel <farleyrunkel@gmail.com>
2025-01-03 11:41:08 +0800

    add BaseObject

--- src/Comm/BaseObject.h
+++ src/Comm/BaseObject.h
@@ -5,20 +5,65 @@
 
 #include <string>
 
-#include <Standard_Transient.hxx>
+#include <boost/signals2.hpp>
+#include <boost/uuid/nil_generator.hpp>
+#include <boost/uuid/uuid.hpp>
+
 #include <Standard_Handle.hxx>
+#include <Standard_Transient.hxx>
 
 namespace sun 
 {
 
-class BaseObject : public Standard_Transient
+class BaseObject : public Standard_Transient 
 {
 public:
-	BaseObject() = default;
+    BaseObject() : SuppressPropertyChangedEvent(false), IsDeserializing(false) {}
 
-	void RaisePropertyChanged(const std::string& prop = "") {}
+    // PropertyChanged signal
+    boost::signals2::signal<void(const std::string&)> PropertyChanged;
+
+    // Property to control event suppression
+    bool SuppressPropertyChangedEvent;
+
+    // To handle the event notification
+    virtual void RaisePropertyChanged(const std::string& propertyName = "") 
+    {
+        if (PropertyChanged.num_slots() != 0 && !SuppressPropertyChangedEvent) {
+            PropertyChanged(propertyName);
+        }
+    }
+
+    // Serialization and deserialization state
+    bool IsDeserializing;
+
+    virtual void OnBeforeUndo() {}
+
+    virtual void OnAfterUndo() {}
+
+    // Serialization methods (placeholders)
+    virtual void OnBeginSerializing() {}
+
+    virtual void OnSerialized() {}
+
+    virtual void OnBeginDeserializing() 
+    {
+        IsDeserializing = true;
+    }
+
+    virtual void OnDeserialized() 
+    {
+        IsDeserializing = false;
+    }
+
+    // Guid equivalent method
+    virtual boost::uuids::uuid GetReferenceId() 
+    {
+        return boost::uuids::nil_uuid();  // Return nil UUID as default
+    }
+
 };
 
-}  // namespace Sun
+}  // namespace sun
 
 #endif  // COMM_BASEOBJECT_H_
